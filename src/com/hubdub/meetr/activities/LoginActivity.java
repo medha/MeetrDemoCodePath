1a3a4a568375a274c071024f4bcace82a5823253
dsampath <dhananjay.sampath@gmail.com>
2013-10-27 15:09:16 -0700

    Added login using facebook. Note that we are not using Facebook SDK directly. We are letting Parse manage the Facebook Login for us so we don't have to correlate two sets of data for the same users - one operated via Facebook SDK and one via Parse SDK. TODO: Needs embellleshing.

---
+++ src/com/hubdub/meetr/activities/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.hubdub.meetr.activities;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.hubdub.meetr.R;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+public class LoginActivity extends Activity {
+
+	private Button loginButton;
+	private Dialog progressDialog;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.login_activity);
+		Parse.initialize(this, "rcJ9OjhbQUqRqos6EusNdnwGEYNC9d4a6rXdqAMU",
+				"3SRkJuZREKUG3bwvMsjYXOsPXqSdzONx6MzaXWAH");
+		// Set your Facebook App Id in strings.xml
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+		ParseAnalytics.trackAppOpened(getIntent());
+
+		loginButton = (Button) findViewById(R.id.loginbutton);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLoginButtonClicked();
+			}
+		});
+
+		// Check if there is a currently logged in user
+		// and they are linked to a Facebook account.
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the user info activity
+			showUserDetailsActivity();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		// getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+	}
+
+	private void onLoginButtonClicked() {
+		LoginActivity.this.progressDialog = ProgressDialog.show(
+				LoginActivity.this, "", "Logging in...", true);
+		List<String> permissions = Arrays.asList("basic_info", "user_about_me",
+				"user_relationships", "user_birthday", "user_location");
+		ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException err) {
+				LoginActivity.this.progressDialog.dismiss();
+				if (user == null) {
+					Log.d("Parse",
+							"Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					Log.d("Parse",
+							"User signed up and logged in through Facebook!");
+					showUserDetailsActivity();
+				} else {
+					Log.d("Parse", "User logged in through Facebook!");
+					showUserDetailsActivity();
+				}
+			}
+		});
+	}
+
+	private void showUserDetailsActivity() {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+}
