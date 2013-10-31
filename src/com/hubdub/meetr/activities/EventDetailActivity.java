e035d99777cf272f41463317a3f0c835ae4d888d
Medha Ghatikesh <medha.ghati@gmail.com>
2013-10-31 13:04:36 -0700

    Inital commit of event detail layout.xml and Activity.java

---
+++ src/com/hubdub/meetr/activities/EventDetailActivity.java
@@ -0,0 +1,17 @@
+package com.hubdub.meetr.activities;
+
+import com.hubdub.meetr.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_event_detail);
+	}
+	
+}
