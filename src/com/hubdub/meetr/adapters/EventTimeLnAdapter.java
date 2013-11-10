368ceaf48e27fcbc331a5a5befb5b8ab00d3a7dd
Medha Ghatikesh <medha.ghati@gmail.com>
2013-11-09 22:21:37 -0800

    adding memory and disk cache for event timeline

--- src/com/hubdub/meetr/adapters/EventTimeLnAdapter.java
+++ src/com/hubdub/meetr/adapters/EventTimeLnAdapter.java
@@ -5,27 +5,35 @@ import java.util.ArrayList;
 import org.json.JSONException;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hubdub.meetr.R;
 import com.hubdub.meetr.models.EventActivity;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseImageView;
 import com.parse.ParseObject;
 
 public class EventTimeLnAdapter extends ArrayAdapter<EventActivity> {
 	Context mContext;
+	private ImageLoadHelper imageLoadHelper;
 
 	public EventTimeLnAdapter(Context context, ArrayList<EventActivity> arrayList) {
 		super(context, 0, arrayList);
 		mContext = context;
 
+		// Using a helper class. Keeps the adapter clean and tidy.
+		imageLoadHelper = new ImageLoadHelper(context);
 	}
 
 	@Override
@@ -37,7 +45,7 @@ public class EventTimeLnAdapter extends ArrayAdapter<EventActivity> {
 		final EventActivity activity = getItem(position);
 		TextView tvPost = (TextView) view.findViewById(R.id.eventPost);
 		TextView tvPostBy = (TextView) view.findViewById(R.id.postBy);
-		ParseImageView imageView = (ParseImageView) view.findViewById(R.id.ivPhotoPost);
+		final ImageView imageView = (ImageView) view.findViewById(R.id.ivPhotoPost);
 		
 		if(activity.getObjectId() != null){
 			
@@ -72,15 +80,11 @@ public class EventTimeLnAdapter extends ArrayAdapter<EventActivity> {
 					ParseObject eventObj = activity.getParseObject("activityFrom");
 					
 					ParseFile photoFile = obj.getParseFile("photo");
+					imageLoadHelper.loadBitmap(photoFile, imageView);
+					
 					System.out.println("getting photo file: " + photoFile.hashCode());
 					tvPostBy.setText(eventObj.getJSONObject("profile").getString("name"));
-
-					 // The placeholder will be used before and during the fetch, to be replaced by the fetched image
-					 // data.
-					 imageView.setPlaceholder(getContext().getResources().getDrawable(R.drawable.transperent_color));
-					 imageView.setParseFile(photoFile);
-					 imageView.loadInBackground();
-					
+	
 				} catch (JSONException e) {
 					Toast.makeText(getContext(), "We weren't able to do that. Sorry!", Toast.LENGTH_SHORT).show();
 					Log.d("ERROR", "4: " + e.toString());
