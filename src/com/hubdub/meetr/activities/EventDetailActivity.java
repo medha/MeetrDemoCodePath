5b791741c4623c9de77cd782143f7e1c80ae6c07
dsampath <dhananjay.sampath@gmail.com>
2013-11-10 10:07:49 -0800

    added swipeable tab views

--- src/com/hubdub/meetr/activities/EventDetailActivity.java
+++ src/com/hubdub/meetr/activities/EventDetailActivity.java
@@ -2,54 +2,58 @@ package com.hubdub.meetr.activities;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
-import android.content.Intent;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
-
 import com.hubdub.meetr.R;
 import com.hubdub.meetr.fragments.EventDetailFragment;
 import com.hubdub.meetr.fragments.EventTimelineFragment;
 
-public class EventDetailActivity extends FragmentActivity {
+public class EventDetailActivity extends FragmentActivity implements ActionBar.TabListener {
+	
+	/* Remeber if this becomes too memory intensive switch to statePagerAdapter */
+    AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+    ViewPager mViewPager;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_detail_frame);
-		setupTabs();
+		//setupTabs();
 		
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-	}
-	
-	private void setupTabs() {
-		ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.setDisplayShowTitleEnabled(true);
-
-		Tab eventDetailTab = actionBar
-			.newTab()
-			.setText("Event details")
-//			.setIcon(R.drawable.)
-			.setTag("EventDetailFragment")
-			.setTabListener(
-				new FragmentTabListener<EventDetailFragment>(R.id.flForEventDetailTabs, this, "EventDetailFragment",
-						EventDetailFragment.class));
-
-		actionBar.addTab(eventDetailTab);
-		actionBar.selectTab(eventDetailTab);
-
-		Tab eventTimelineTab = actionBar
-			.newTab()
-			.setText("Event Timeline")
-//			.setIcon(R.drawable.ic_mentions)
-			.setTag("EventTimelineFragment")
-			.setTabListener(
-			    new FragmentTabListener<EventTimelineFragment>(R.id.flForEventDetailTabs, this, "EventTimelineFragment",
-			    		EventTimelineFragment.class));
-
-		actionBar.addTab(eventTimelineTab);
+        mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
+        final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // When swiping between different app sections, select the corresponding tab.
+                // We can also use ActionBar.Tab#select() to do this if we have a reference to the
+                // Tab.
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+        
+        actionBar.addTab(actionBar.newTab()
+                        .setText("Details")
+                        .setTabListener(this));
+        
+        actionBar.addTab(actionBar.newTab()
+                .setText("Timeline")
+                .setTabListener(this));
+        
+        actionBar.addTab(actionBar.newTab()
+                .setText("Photos")
+                .setTabListener(this));
 	}
 	
 	@Override
@@ -62,5 +66,54 @@ public class EventDetailActivity extends FragmentActivity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        // When the given tab is selected, switch to the corresponding page in the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		
+	}
+    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public AppSectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+        	Fragment fragment = null;
+            switch (i) {
+                case 0:
+                	fragment = new EventDetailFragment();
+                    return fragment;
+                case 1:
+                	fragment = new EventTimelineFragment();
+                    return fragment;
+                    
+                case 2:
+                	fragment = new EventTimelineFragment();
+                	return fragment;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Section " + (position + 1);
+        }
+    }
+
 }
